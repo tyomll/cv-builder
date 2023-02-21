f46ce3af97515af52d1b80ccbe52bff03b7df884
tyommmm <artyomhovsepyan135@gmail.com>
2023-02-21 21:32:48 +0400

    Add: Home page component and design.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import cvVector from '../../assets/home-cv-vector.png';
 import s from './Home.module.scss';
 
 const Home = () => {
-  return <div className={s.root}></div>;
+  return (
+    <div className={s.root}>
+      <div className={s.container}>
+        <div className={s.heading}>
+          <h1>Your Dream Job Starts Here.</h1>
+          <h2>Build Your Resume Now!</h2>
+          <p>
+            Welcome to Resumize, the easiest way to create a professional CV that stands out from
+            the crowd. Our powerful yet simple-to-use tool helps you create a custom CV in minutes,
+            with no design skills needed.
+          </p>
+          <button>Start Building CV</button>
+        </div>
+        <div className={s.image}>
+          <img src={cvVector} alt="cv" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
